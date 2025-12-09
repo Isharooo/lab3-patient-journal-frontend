3c8ae9040113deebfbfbddced3c64536da93dc62
Majd Matloob <Majd.matloob04@gmail.com>
2025-12-09 19:11:11 +0100

    test5

--- src/keycloak.js
+++ src/keycloak.js
@@ -14,7 +14,6 @@ let initPromise = null;
 
 export const initKeycloak = () => {
   console.log('initKeycloak called, initPromise exists:', !!initPromise);
-  console.log('keycloak.authenticated:', keycloak.authenticated);
 
   if (initPromise) {
     console.log('Returning existing promise');
@@ -24,12 +23,18 @@ export const initKeycloak = () => {
   console.log('Starting new init');
   initPromise = keycloak
       .init({
-        onLoad: 'login-required',
+        onLoad: 'check-sso',
         checkLoginIframe: false,
         pkceMethod: 'S256',
+        silentCheckSsoRedirectUri: window.location.origin + '/silent-check-sso.html',
       })
       .then((authenticated) => {
         console.log('Init completed, authenticated:', authenticated);
+        if (!authenticated) {
+          console.log('Not authenticated, redirecting to login');
+          keycloak.login();
+          return false;
+        }
         if (window.location.search.includes('code=') || window.location.search.includes('state=')) {
           console.log('Clearing URL params');
           window.history.replaceState({}, document.title, window.location.pathname);
